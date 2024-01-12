6cd2e2d13234c629fcc172acb1ecf2ec80c849fc
obrucheoghene <wilfredcloudspace@gmail.com>
2024-01-12 02:10:30 +0100

    feat: add redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 import "antd/dist/reset.css"
-import RoomProvider from './contexts/RoomContext.tsx'
-import AuthProvider from './contexts/AuthContext.tsx'
+import RoomProvider from './contexts/RoomContext'
+import AuthProvider from './contexts/AuthContext'
+import { Provider } from 'react-redux'
+import store from './store.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <RoomProvider>
+        <Provider store={store}>
         <App />
+        </Provider>
+       
       </RoomProvider>
     </AuthProvider>
   </React.StrictMode>,
